7ec3a787fa6906f2c1e9a697a2791988963f9f39
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-04-06 10:13:41 +0900

    refacotr

--- src/main/java/love/chihuyu/skylife/gacha/GachaCommand.kt
+++ src/main/java/love/chihuyu/skylife/gacha/GachaCommand.kt
@@ -1,32 +1,33 @@
 package love.chihuyu.skylife.gacha
 
 import love.chihuyu.skylife.Command
-import love.chihuyu.skylife.util.ItemUtil
-import org.bukkit.ChatColor
-import org.bukkit.Material
+import love.chihuyu.skylife.data.GachaData
+import love.chihuyu.skylife.data.GachaDataRecord
+import love.chihuyu.skylife.util.addOrDropItem
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 object GachaCommand : Command("gacha") {
     override fun onCommand(sender: CommandSender, label: String, args: Array<out String>) {
-        if (sender !is Player || !sender.hasPermission("skylife.command.gacha") || args.size > 2) return
+        if (!(sender is Player && sender.hasPermission("skylife.command.gacha") && args.size <= 4)) return
+        if (args[0] != "勤労感謝ガチャ") return
 
-        when (args[0]) {
-            "give" -> {
-                when (args[1]) {
-                    "勤労感謝ガチャ" -> {
-                        val item = ItemUtil.create(Material.DRAGON_HEAD,
-                            ChatColor.AQUA.toString() + "-=+ 勤労感謝ガチャ +=-",
-                            lore = listOf("勤勉な者のみが手にできるガチャ券"))
-
-                        sender.inventory.addItem(item)
-                    }
-                }
-            }
+        val target = sender.server.onlinePlayers.find { it.displayName == args[1] } ?: return
+        val gacha = GachaData::class.memberProperties.find { it.name == args[2] }?.get(GachaData) as GachaDataRecord
+        val amount = args[3].toIntOrNull() ?: 1
+        repeat(amount) {
+            target.inventory.addOrDropItem(gacha.getGachaItem(amount))
         }
     }
 
-    override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String> {
-        return emptyList()
+    override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String>? {
+        val players = sender.server.onlinePlayers.toTypedArray()
+        // val gachaNames = GachaData.keys.toList()
+
+        return when (args.size) {
+            1 -> players.map { it.displayName }
+            // 2 -> gachaNames
+            else -> null
+        }
     }
 }
