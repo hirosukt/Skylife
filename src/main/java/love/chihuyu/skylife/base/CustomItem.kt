280170560a871b274bc984d1e9dcce1223ce6407
Tenzyu Masuda <tenzyu.on@gmail.com>
2022-04-11 16:38:56 +0900

    revert file name

--- src/main/java/love/chihuyu/skylife/base/CustomItem.kt
+++ src/main/java/love/chihuyu/skylife/base/CustomItem.kt
@@ -3,7 +3,7 @@ package love.chihuyu.skylife.base
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
-interface CustomHeadItem {
+interface CustomItem {
     val material: Material
     val name: String
     fun getItem(amount: Int): ItemStack
