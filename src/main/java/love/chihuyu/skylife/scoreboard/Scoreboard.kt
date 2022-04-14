208b6f9011cc3fc8e065d9aa3204d60eea670105
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-04-14 16:47:33 +0900

    change design scoreboard

--- src/main/java/love/chihuyu/skylife/scoreboard/Scoreboard.kt
+++ src/main/java/love/chihuyu/skylife/scoreboard/Scoreboard.kt
@@ -1,9 +1,12 @@
 package love.chihuyu.skylife.scoreboard
 
+import love.chihuyu.skylife.Skylife.Companion.plugin
 import love.chihuyu.skylife.database.Stats
 import love.chihuyu.skylife.database.Users
 import love.chihuyu.skylife.util.background
+import net.md_5.bungee.api.ChatColor.*
 import org.bukkit.Bukkit.getScoreboardManager
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.scoreboard.DisplaySlot
 import org.jetbrains.exposed.sql.select
@@ -12,7 +15,7 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 object Scoreboard {
     fun update(player: Player) {
         val name = "skylife_stats_${player.uniqueId}"
-        val displayName = "§l§bSKYLIFE§7 | §astats  "
+        val displayName = "${ChatColor.AQUA}${ChatColor.BOLD}${ChatColor.ITALIC}SKYLIFE ${ChatColor.GRAY}v0.1"
         val manager = getScoreboardManager() ?: return
         val scoreboard = manager.newScoreboard
         val objective = scoreboard.getObjective(name)
@@ -32,15 +35,15 @@ object Scoreboard {
                 val nextSyokuryoGacha = perSyokuryoGacha - (foodConsumed % perSyokuryoGacha)
                 val nextKenzaiGacha = perKenzaiGacha - (blockPlaced % perKenzaiGacha)
                 objective.apply {
-                    getScore("§6§l╋━━━━━━━━━━━━━━━━━╋§r").score = 8
-                    getScore("       ").score = 7
-                    getScore("      ").score = 6
-                    getScore("     ").score = 5
-                    getScore("    ").score = 4
-                    getScore("食料ガチャまで $nextSyokuryoGacha").score = 3
-                    getScore("建材ガチャまで $nextKenzaiGacha").score = 2
+                    getScore("$GOLD$STRIKETHROUGH━━━━━━━━━━━━━━━━━━━$RESET").score = 8
+                    getScore("   ").score = 7
+                    getScore("プレイヤー数: ${plugin.server.onlinePlayers.size}").score = 6
+                    getScore("  ").score = 5
+                    getScore("次のガチャまで").score = 4
+                    getScore("食 $GRAY> $WHITE$nextSyokuryoGacha${GRAY}個").score = 3
+                    getScore("建 $GRAY> $WHITE$nextKenzaiGacha${GRAY}ブロック").score = 2
                     getScore(" ").score = 1
-                    getScore("§6§l╋━━━━━━━━━━━━━━━━━╋").score = 0
+                    getScore("$GOLD$STRIKETHROUGH━━━━━━━━━━━━━━━━━━━").score = 0
                     displaySlot = DisplaySlot.SIDEBAR
                 }
             }
